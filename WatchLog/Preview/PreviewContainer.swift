b5599d41903ebdeb3cd4e90be647e3a131a0e20f
Marcus Hoerning <hr75ma>
2025-05-21 11:17:41 +0200

    preview data

--- WatchLog/Preview/PreviewContainer.swift
+++ WatchLog/Preview/PreviewContainer.swift
@@ -24,6 +24,47 @@ struct PreviewData {
 
   func addExampleData() {
 
+      Task { @MainActor in
+          
+          var currentDate:Date = Date()
+          let dateFormatter = DateFormatter()
+          dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+          var futureDate: Date = Date()
+          
+          for year in 0...1 {
+          futureDate = Calendar.current.date(byAdding: .year, value: year, to: currentDate)!
+          let yearEntry = WatchLogBookYear(LogDate: futureDate)
+          modelContainer.mainContext.insert(yearEntry)
+              
+              for month in 0...2 {
+                  print(month)
+                  
+                  futureDate = Calendar.current.date(byAdding: .month, value: month, to: currentDate)!
+                  print(futureDate)
+                  let monthEntry = WatchLogBookMonth(LogDate: futureDate)
+                  modelContainer.mainContext.insert(monthEntry)
+                  yearEntry.Children?.append(monthEntry)
+                  
+                  for day in 0...1 {
+                      futureDate = Calendar.current.date(byAdding: .day, value: day, to: currentDate)!
+                      let dayEntry = WatchLogBookDay(LogDate: futureDate)
+                      modelContainer.mainContext.insert(dayEntry)
+                      monthEntry.Children?.append(dayEntry)
+                      
+                      for entry in 1...2 {
+                          let entryObject = WatchLogBookEntry(LogEntry: WatchLogEntry())
+                          modelContainer.mainContext.insert(entryObject)
+                          dayEntry.Children?.append(entryObject)
+                          
+                      }
+                  }
+              }
+          }
+      }
+      
+      
+      
+      
 //    Task { @MainActor in
 //
 //      for y in 2025...2026 {
